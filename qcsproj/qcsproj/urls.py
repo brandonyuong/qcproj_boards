cbc96178b86e87e9d7d7248556821a96542b547a
Brandon Yuong <brandonyuong@gmail.com>
2019-04-16 10:23:42 -0700

    Adding authentication.

--- qcsproj/qcsproj/urls.py
+++ qcsproj/qcsproj/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 
+from accounts import views as accounts_views
 from boards import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    re_path(r'^signup/$', accounts_views.signup, name='signup'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 ]
