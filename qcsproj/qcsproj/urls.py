2717f27de6a0d10aca54a88f3560ac4c03772c14
Brandon Yuong <brandonyuong@gmail.com>
2019-04-16 16:01:06 -0700

    Implemented password change.

--- qcsproj/qcsproj/urls.py
+++ qcsproj/qcsproj/urls.py
@@ -24,6 +24,10 @@ from boards import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
+    re_path(r'^settings/password/$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+    name='password_change'),
+    re_path(r'^settings/password/done/$', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+    name='password_change_done'),
     re_path(r'^reset/$',
         auth_views.PasswordResetView.as_view(
             template_name='password_reset.html',
