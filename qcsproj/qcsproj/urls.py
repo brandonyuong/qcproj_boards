616d7d83bd8c6010f3d56e570c8d69d5e8afc20f
Brandon Yuong <brandonyuong@gmail.com>
2019-04-19 16:27:43 -0700

    Added Reply Topic pages.

--- qcsproj/qcsproj/urls.py
+++ qcsproj/qcsproj/urls.py
@@ -31,7 +31,8 @@ urlpatterns = [
             auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
             name='password_change'),
     re_path(r'^settings/password/done/$',
-            auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+            auth_views.PasswordChangeDoneView.as_view(
+                template_name='password_change_done.html'),
             name='password_change_done'),
 
     re_path(r'^reset/$',
@@ -41,14 +42,17 @@ urlpatterns = [
                 subject_template_name='password_reset_subject.txt'
             ), name='password_reset'),
     re_path(r'^reset/done/$',
-        auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
-        name='password_reset_done'),
+            auth_views.PasswordResetDoneView.as_view(
+                template_name='password_reset_done.html'),
+            name='password_reset_done'),
     re_path(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
-        name='password_reset_confirm'),
+            auth_views.PasswordResetConfirmView.as_view(
+                template_name='password_reset_confirm.html'),
+            name='password_reset_confirm'),
     re_path(r'^reset/complete/$',
-        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
-        name='password_reset_complete'),
+            auth_views.PasswordResetCompleteView.as_view(
+                template_name='password_reset_complete.html'),
+            name='password_reset_complete'),
 
     re_path(r'^signup/$', accounts_views.signup, name='signup'),
     re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'),
@@ -56,4 +60,6 @@ urlpatterns = [
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$',
+            views.reply_topic, name='reply_topic'),
 ]
