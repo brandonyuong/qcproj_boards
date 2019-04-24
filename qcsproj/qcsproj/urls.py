a5b74a1ebd9b22afd5717a2a0e972ac9c2515f11
Brandon Yuong <brandonyuong@gmail.com>
2019-04-24 11:10:04 -0700

    Implemented GCBV pagination, reusable pagination template.

--- qcsproj/qcsproj/urls.py
+++ qcsproj/qcsproj/urls.py
@@ -24,8 +24,7 @@ from boards import views
 urlpatterns = [
     re_path(r'^$', views.BoardListView.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts,
-            name='topic_posts'),
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
 
     re_path(r'^settings/password/$',
             auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
@@ -58,11 +57,10 @@ urlpatterns = [
     re_path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'),
             name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    re_path(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$',
             views.reply_topic, name='reply_topic'),
     re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
             views.PostUpdateView.as_view(), name='edit_post'),
-
 ]
