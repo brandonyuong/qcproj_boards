7d70f43c186642b788df8377faf1bd5e12516733
Brandon Yuong <brandonyuong@gmail.com>
2019-04-26 14:57:14 -0700

    Finishing touches

--- qcsproj/boards/views.py
+++ qcsproj/boards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Board, Topic, Post
 from django.contrib.auth.models import User
 from .forms import NewTopicForm, PostForm
@@ -37,12 +37,16 @@ class PostListView(ListView):
     model = Post
     context_object_name = 'posts'
     template_name = 'topic_posts.html'
-    paginate_by = 2
+    paginate_by = 20
 
     def get_context_data(self, **kwargs):
-        # TODO add topic view counter
-        # self.topic.views += 1
-        self.topic.save()
+        session_key = 'viewed_topic_{}'.format(self.topic.pk)  # <-- here
+        if not self.request.session.get(session_key, False):
+            # TODO add topic view counter
+            # self.topic.views += 1
+            self.topic.save()
+            self.request.session[session_key] = True  # <-- until here
+
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
 
@@ -83,7 +87,18 @@ def reply_topic(request, pk, topic_pk):
             post.topic = topic
             post.created_by = request.user
             post.save()
-            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+
+            topic.last_updated = timezone.now()
+            topic.save()
+
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
